45006cc0d5c63ad99d0972a781f1d3b20570b24b
Administrator <admin@example.com>
2020-05-02 02:00:08 +0900

    Initial commit

---
+++ app/src/main/java/com/example/dontpicktup/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.dontpicktup
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        toggle.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                val intent = Intent(this, AlarmService::class.java)
+                startService(intent)
+            } else {
+                val intent = Intent(this, AlarmService::class.java)
+                stopService(intent)
+            }
+        }
+    }
+}
